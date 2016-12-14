a69cd01a62e3c756167a0f7f8c852db5a9077600
lmarti3849 <lmarti3849@WS-AC-CTD-37008>
2016-12-14 08:09:40 -0500

    Add project files.

---
+++ MusicStoreLance/Models/Album.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MusicStoreLance.Models
+{
+    public class Album
+    {
+        public int AlbumId { get; set; }
+
+        public string Title { get; set; }
+
+        public decimal Price { get; set; }
+    }
+
+    public class MusicContext : DbContext
+    {
+        public DbSet<Album> Album { get; set; }
+    }
+}
